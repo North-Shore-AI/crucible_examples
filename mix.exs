4be5702a17d659e87bc74cfa97a2255460d5b1db
nshkrdotcom <127063941+nshkrdotcom@users.noreply.github.com>
2026-07-15 14:46:10 -1000

    deps: refresh Hex dependencies

--- mix.exs
+++ mix.exs
@@ -42,13 +42,13 @@ defmodule CrucibleExamples.MixProject do
     [
       # Phoenix Framework
       {:phoenix, "~> 1.8.1"},
-      {:phoenix_html, "~> 4.1"},
-      {:phoenix_live_reload, "~> 1.2", only: :dev},
-      {:phoenix_live_view, "~> 1.1.0"},
-      {:lazy_html, ">= 0.1.0", only: :test},
-      {:phoenix_live_dashboard, "~> 0.8.3"},
-      {:esbuild, "~> 0.10", runtime: Mix.env() == :dev},
-      {:tailwind, "~> 0.3", runtime: Mix.env() == :dev},
+      {:phoenix_html, "~> 4.3"},
+      {:phoenix_live_reload, "~> 1.6", only: :dev},
+      {:phoenix_live_view, "~> 1.2"},
+      {:lazy_html, "~> 0.1.11", only: :test},
+      {:phoenix_live_dashboard, "~> 0.8.7"},
+      {:esbuild, "~> 0.10.0", runtime: Mix.env() == :dev},
+      {:tailwind, "~> 0.5.1", runtime: Mix.env() == :dev},
       {:heroicons,
        github: "tailwindlabs/heroicons",
        tag: "v2.2.0",
@@ -56,27 +56,29 @@ defmodule CrucibleExamples.MixProject do
        app: false,
        compile: false,
        depth: 1},
-      {:telemetry_metrics, "~> 1.0"},
-      {:telemetry_poller, "~> 1.0"},
-      {:gettext, "~> 0.26"},
-      {:jason, "~> 1.2"},
+      {:telemetry_metrics, "~> 1.1"},
+      {:telemetry_poller, "~> 1.3"},
+      {:gettext, "~> 1.0"},
+      {:jason, "~> 1.4"},
       {:dns_cluster, "~> 0.2.0"},
-      {:bandit, "~> 1.5"},
+      {:bandit, "~> 1.12"},
 
       # Crucible Framework - Local path dependencies
-      {:crucible_bench, path: "../crucible_bench"},
-      {:crucible_ensemble, path: "../crucible_ensemble"},
-      {:crucible_hedging, path: "../crucible_hedging"},
-      {:crucible_telemetry, path: "../crucible_telemetry"},
-      {:crucible_trace, path: "../crucible_trace"},
-      {:crucible_harness, path: "../crucible_harness"},
-      {:crucible_datasets, path: "../crucible_datasets"},
+      {:crucible_framework, path: "../crucible_framework", override: true},
+      {:crucible_bench, path: "../crucible_bench", override: true},
+      {:crucible_ensemble, path: "../crucible_ensemble", override: true},
+      {:crucible_hedging, path: "../crucible_hedging", override: true},
+      {:crucible_telemetry, path: "../crucible_telemetry", override: true},
+      {:crucible_trace, path: "../crucible_trace", override: true},
+      {:crucible_harness, path: "../crucible_harness", override: true},
+      {:crucible_datasets, path: "../crucible_datasets", override: true},
+      {:crucible_ir, "~> 0.3.0", override: true},
 
       # Visualization & Charts
       {:contex, "~> 0.5"},
 
       # Mock Data Generation
-      {:faker, "~> 0.18"}
+      {:faker, "~> 0.19.0"}
     ]
   end
 
